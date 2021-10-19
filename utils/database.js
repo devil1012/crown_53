b633d43d2c2048c56a55f68e305d28c99effd4d4
Corvus <37375644+devil1012@users.noreply.github.com>
2021-10-19 19:07:56 +0800

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,14 +1,18 @@
 const { Pool } = require('pg')
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'crown_53',
-  password: '0000',
-  port: 5432,
-})
-
-pool.query('SELECT * from category',(err,res)=>{
-    console.log(JSON.stringify(res.rows));
-    pool.end();
-});
+const isProduction=process.env.NODE_KEY==='production';
+let pool;
+if(isProduction){
+  pool=new ({
+    connectionString:process.env.DATABASE_URL,
+    ssl:{rejectUnauthorized:false},
+  });
+}else{
+  pool=new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'crown_53',
+    password: '0000',
+    port: 5432,
+  })
+}
 module.exports = pool;
